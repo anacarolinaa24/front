3b03289679f25af22a031fea3e649aade1e155fa
Carolina <anacarolinasilva17@outlook.com>
2025-03-21 21:14:39 -0300

    segundo commit

---
+++ projetofrontand/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import logoSite from "../assets/logo.png";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="paginaInicial">
+      <img src={logoSite} alt="logo" />
+    </div>
+  );
+};
+
+export default Home;
